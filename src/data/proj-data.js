586001496360adec77b6cfa6d7b175005d3cda8a
Claire Dranginis <cdranginis@gmail.com>
2016-12-01 11:17:31 -0500

    add images and data

---
+++ src/data/proj-data.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: 'traVerse',
+    description: 'A journalling site that allows you to take a journey through a 3D landscape created from your past thoughts and emotions',
+    img: '/images/traverse1.png'
+  },
+  {
+    name: 'Fetchd',
+    description: 'A novel way to find an adoptable dog in your area',
+    img: '/images/fetchd.png'
+  }
+]
