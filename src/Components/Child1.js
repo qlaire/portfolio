a6102e27365df693d0049c64ac6a1a4b90a3f6e4
Claire Dranginis <cdranginis@gmail.com>
2016-12-01 18:14:38 -0500

    initial commit for attempt 2

--- src/Components/Child1.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default () => (
-  <div>
-    <h1>Child 1</h1>
-  </div>
-);
--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducers';
-import thunkMiddleware from 'redux-thunk';
-import createLogger from 'redux-logger';
-
-export default createStore(
-  rootReducer,
-  applyMiddleware(thunkMiddleware, createLogger())
-);
