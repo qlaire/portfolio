1efee06615f33a5735a050c046eca0b4e689cc0a
Claire Dranginis <cdranginis@gmail.com>
2016-12-08 17:06:08 -0500

    added experience

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,24 @@ $('a[href^="#"]').on('click', function(event) {
         }, 1000);
     }
 });
+
+$('ul.about-nav .btn').on('click', function () {
+    if ($(this).hasClass('selected')) {
+        return;
+    } else {
+        $('ul.about-nav .btn').removeClass('selected');
+        $(this).addClass('selected');
+        $('#about-2 > div').hide();
+        switch ($(this).text()) {
+            case 'Experience':
+                $('.experience').show();
+                break;
+            case 'Skills':
+                $('.skills').show();
+                break;
+            case 'Resume':
+                $('.resume').show();
+                break;
+        }
+    }
+});
